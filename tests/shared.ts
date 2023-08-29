491afe876aa24fb5ff1852776e641bd97e68a50a
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-29 10:14:57 +0200

    Print env variables to make sure

--- tests/shared.ts
+++ tests/shared.ts
@@ -6,6 +6,10 @@ export const host = process.env.HOST;
 const user = process.env.USER as string;
 const password = process.env.PW as string;
 
+console.log(`Host: ${host}`);
+console.log(`User: ${user}`);
+console.log(`PW: ${password}`);
+
 export const login = async (page: Page) => {
 	await page.goto(`${host}/Account/Login`);
 	await page.locator('#UserName').fill(user);
