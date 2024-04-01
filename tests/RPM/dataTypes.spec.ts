883f3c6c282f2593729dd64301db05037427c85e
Ummaima Saleem <149822406+UmmaimaSaleem44@users.noreply.github.com>
2024-04-01 20:04:09 +0200

    Adding slow statements to dataTypes.spec.ts #4
    Adding slow statements to handle slow rendering.

--- tests/RPM/dataTypes.spec.ts
+++ tests/RPM/dataTypes.spec.ts
@@ -23,22 +23,18 @@ test.describe('Data Types', () => {
     test.afterAll(async () => {
         await page.close(); // Close the page after all tests
     });
-
+    test.slow();
     test('Title', async () => {
         // Check the title of the table
         const title = await page
             .locator('.w-full', { has: page.locator('.table.table-compact') })
-            .locator('h1.h1');
+            .locator('h1');
         await expect(title).toHaveText('Data Types');
     });
 
     // Unique name for the data type
     test.describe('Iterations for fields validation', () => {
         const dataTypeIterate = `test_iterate-${+Date.now()}`;
-        test('Check title ', async () => {
-            //Check with title name
-            await checkTitle(page, 'Data Types', '.w-full >> .table.table-compact', 'h1.h1');
-        });
         const dataTypes = [
             "Empty",
             "Object",
@@ -65,13 +61,12 @@ test.describe('Data Types', () => {
             });
         });
 
-
-
         test('Check with name field and description', async () => {
             await checkDataTypes(page, dataTypeIterate, "Testing", "")
         });
 
     });
+    test.slow();
     test.describe('Create new data type', () => {
         const dataType = `test_new-${+Date.now()}`;
         test('should match the expected title', async () => {
@@ -81,7 +76,7 @@ test.describe('Data Types', () => {
         test('Create data type', async () => {
             await createDataType(page, dataType, "Empty");
         });
-
+        //test.slow()
         test('Find the new data type in the table', async () => {
             await findDataType(page, dataType);
         });
