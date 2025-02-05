ffa2fbbf417da6e98f5128f7f47d129a0b3efd3c
Ummaima Saleem <149822406+UmmaimaSaleem44@users.noreply.github.com>
2025-02-05 17:11:25 +0100

    Add files via upload
    Updated the index number for the different patterns

--- tests/RPM/createTabularData.spec.ts
+++ tests/RPM/createTabularData.spec.ts
@@ -58,91 +58,91 @@ test.describe('Data Types', () => {
           
 
         test('Assigning the data type (date) and display pattern (dd.MM.yy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,1,'date','dd.MM.yy');
+            await  AssignDataTypeDisplayPattern(page,0,'date','dd.MM.yy');
           });
           
 
         test('Assigning the data type (date) and display pattern (dd.MM.yyyy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,2,'date','dd.MM.yyyy');
+            await  AssignDataTypeDisplayPattern(page,1,'date','dd.MM.yyyy');
           });
           
 
         test('Assigning the data type (date) and display pattern (dd/MM/yy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,3,'date','dd/MM/yy');
+            await  AssignDataTypeDisplayPattern(page,2,'date','dd/MM/yy');
           });
           
 
         test('Assigning the data type (date) and display pattern (dd/MM/yyyy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,4,'date','dd/MM/yyyy');
+            await  AssignDataTypeDisplayPattern(page,3,'date','dd/MM/yyyy');
           });
          
 
         test('Assigning the data type (date) and display pattern (M/d/yyyy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,5,'date','M/d/yyyy');
+            await  AssignDataTypeDisplayPattern(page,4,'date','M/d/yyyy');
           });
           
         
         test('Assigning the data type (datetime) and display pattern (M/d/yyyy h:m)', async () => {
-            await  AssignDataTypeDisplayPattern(page,6,'datetime','M/d/yyyy h:m');
+            await  AssignDataTypeDisplayPattern(page,5,'datetime','M/d/yyyy h:m');
           });
           
 
         test('Assigning the data type (date) and display pattern (MM/dd/yy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,7,'date','MM/dd/yy');
+            await  AssignDataTypeDisplayPattern(page,6,'date','MM/dd/yy');
           });
           
 
         test('Assigning the data type (date) and display pattern (MM/dd/yyyy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,8,'date','MM/dd/yyyy');
+            await  AssignDataTypeDisplayPattern(page,7,'date','MM/dd/yyyy');
           });
          
 
         test('Assigning the data type (date) and display pattern (yy-MM-dd)', async () => {
-            await  AssignDataTypeDisplayPattern(page,9,'date','yy-MM-dd');
+            await  AssignDataTypeDisplayPattern(page,8,'date','yy-MM-dd');
           });
           
 
         test('Assigning the data type (date) and display pattern (yyyy-dd-MM)', async () => {
-            await  AssignDataTypeDisplayPattern(page,10,'date','yyyy-dd-MM');
+            await  AssignDataTypeDisplayPattern(page,9,'date','yyyy-dd-MM');
           });
           
 
         test('Assigning the data type (date) and display pattern (yyyy-d-M)', async () => {
-            await  AssignDataTypeDisplayPattern(page,11,'date','yyyy-d-M');
+            await  AssignDataTypeDisplayPattern(page,10,'date','yyyy-d-M');
           });
           
 
         test('Assigning the data type (date) and display pattern (yyyy-M-d)', async () => {
-            await  AssignDataTypeDisplayPattern(page,12,'date','yyyy-M-d');
+            await  AssignDataTypeDisplayPattern(page,11,'date','yyyy-M-d');
           });
          
 
         test('Assigning the data type (date) and display pattern (yyyy-MM-dd)', async () => {
-            await  AssignDataTypeDisplayPattern(page,13,'date','yyyy-MM-dd');
+            await  AssignDataTypeDisplayPattern(page,12,'date','yyyy-MM-dd');
           });
          
 
         test('Assigning the data type (time) and display pattern (HH:mm)', async () => {
-            await  AssignDataTypeDisplayPattern(page,14,'time','HH:mm');
+            await  AssignDataTypeDisplayPattern(page,13,'time','HH:mm');
           });
           
 
         test('Assigning the data type (time) and display pattern (hh:mm tt)', async () => {
-            await  AssignDataTypeDisplayPattern(page,15,'time','hh:mm tt');
+            await  AssignDataTypeDisplayPattern(page,14,'time','hh:mm tt');
           });
           
 
         test('Assigning the data type (time) and display pattern (hh:mm:ss)', async () => {
-            await  AssignDataTypeDisplayPattern(page,16,'time','hh:mm:ss');
+            await  AssignDataTypeDisplayPattern(page,15,'time','hh:mm:ss');
           });
          
         test('Assigning the data type (time) and display pattern (hh:mm:ss tt)', async () => {
-            await  AssignDataTypeDisplayPattern(page,17,'time','hh:mm:ss tt');
+            await  AssignDataTypeDisplayPattern(page,16,'time','hh:mm:ss tt');
           });
 
         test('Assigning the data type (date) and display pattern (MM)', async () => {
-            await  AssignDataTypeDisplayPattern(page,18,'date','MM');
-            const setDisplayPattern =  page.locator(`:nth-child(18) > #variable-0-container-info > .card .svelte-select > .value-container > #displayPattern`);
+            await  AssignDataTypeDisplayPattern(page,17,'date','MM');
+            const setDisplayPattern =  page.locator('#displayPattern-17');
             setDisplayPattern.press('ArrowDown');
             await page.waitForTimeout(500);
             setDisplayPattern.press('Enter');
@@ -150,8 +150,8 @@ test.describe('Data Types', () => {
           
 
         test('Assigning the data type (date) and display pattern (yyyy)', async () => {
-            await  AssignDataTypeDisplayPattern(page,19,'date','yyyy');
-            const setDisplayPattern =  page.locator(`:nth-child(19) > #variable-0-container-info > .card .svelte-select > .value-container > #displayPattern`);
+            await  AssignDataTypeDisplayPattern(page,18,'date','yyyy');
+            const setDisplayPattern =  page.locator('#displayPattern-18');
             setDisplayPattern.press('ArrowDown');
             await page.waitForTimeout(500);
             setDisplayPattern.press('Enter');
@@ -168,6 +168,7 @@ test.describe('Data Types', () => {
 
         test('Submit the data valdate the validation messages', async () => {
             await  submitData(page)
+            await page.waitForTimeout(2000);
           });
 
     });
